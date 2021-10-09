c24abc8fc72a77f5b7d1eecbfebbfeced64b7b75
amine <amine.bensalah@intech-consulting.fr>
2021-10-09 11:07:10 +0200

    feat: add theme color on safari header (#11)
    * feat: add theme color on safari header
    
    * feat: upgrade to swift 5.4
    
    * fix: unittest

--- Package.swift
+++ Package.swift
@@ -1,17 +1,17 @@
-// swift-tools-version:5.2
+// swift-tools-version:5.4
 
 import PackageDescription
 
 let package = Package(
-    name: "BlogPublish",
+    name: "blog-publish",
     products: [
         .executable(
-            name: "BlogPublish",
+            name: "blog-publish",
             targets: ["BlogPublish"]
         )
     ],
     dependencies: [
-        .package(name: "Publish", url: "https://github.com/johnsundell/publish.git", from: "0.6.0"),
+        .package(name: "Publish", url: "https://github.com/johnsundell/publish.git", from: "0.8.0"),
         .package(name: "SplashPublishPlugin", url: "https://github.com/johnsundell/splashpublishplugin", from: "0.1.0")
     ],
     targets: [
